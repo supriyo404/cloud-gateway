ffe8067f35c95b061d15a3345ba2cea07f00c5e1
supriyo <er.supriya93@gmail.com>
2022-06-02 23:50:43 +0530

    first commit

---
+++ src/main/java/com/api/gateway/routeconfig/RouterConfig.java
@@ -0,0 +1,22 @@
+package com.api.gateway.routeconfig;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouterConfig {
+	
+	@Bean
+	public RouteLocator customAppRouteLocator(RouteLocatorBuilder builder) {
+		
+		return builder.routes()
+				.route("order-service",r->r.path("/api/order/**")
+						.uri("lb://ORDER-SERVICE"))
+				.route("payment-service",i->i.path("/api/payment/**")
+						.uri("lb://PAYMENT-SERVICE"))
+				.build();
+	}
+
+}
